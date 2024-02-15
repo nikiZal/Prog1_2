0f97f07b7a81fb3ea1339e1ec8818d93cf972c4d
bananbka <bananbka0.2@gmail.com>
2024-02-15 04:24:50 +0200

    finished lab2

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <set>
+
 using namespace std;
 
 // База даних фільмів
@@ -10,18 +10,29 @@ class Film {
 private:
     string name;
     int duration;
-    set<string> genres;
     double rating;
 public:
-    void setName () {
-
+    void setName (string newName) {
+        name = newName;
     }
-    void setRating () {
-
+    void setRating (double newRating) {
+        rating = newRating;
     }
     void getAll () {
-        
+        cout << "Name: " << name << endl
+             << "Duration: " << duration << endl
+             << "Rating: " << rating << endl;
     }
+
+    Film () 
+        : Film{"None", 0, 0.0} {};
+    Film (string newName) 
+        : Film{newName, 0, 0.0} {};
+    Film (string newName, int newDuration) 
+        : Film{newName, newDuration, 0.0} {};
+    Film(string newName, int newDuration, double newRating) 
+        : name{newName}, duration{newDuration}, rating{newRating} {};
+    ~Film() {};
 };
 
 class Serial {
@@ -29,29 +40,42 @@ private:
     string name;
     int episodDuration;
     int numberOfEpisodes;
-    set<string> genres;
     double rating;
 public:
-    void setName () {
-
+    void setName (string newName) {
+        name = newName;
     }
-    void setRating () {
-
+    void setRating (double newRating) {
+        rating = newRating;
     }
     void getAll () {
-        
+        cout << "Name: " << name << endl
+             << "Episodes: " << numberOfEpisodes << endl
+             << "Duration: " << episodDuration << endl
+             << "Rating: " << rating << endl;
     }
+
+    Serial () 
+        : Serial{"None", 0, 0, 0.0} {};
+    Serial (string newName) 
+        : Serial{newName, 0, 0, 0.0} {};
+    Serial (string newName, int newNumberOfEpisodes) 
+        : Serial{newName, newNumberOfEpisodes, 0, 0.0} {};
+    Serial (string newName, int newNumberOfEpisodes, int newEpisodDuration) 
+        : Serial{newName, newNumberOfEpisodes, newEpisodDuration, 0.0} {};
+    Serial(string newName, int newNumberOfEpisodes, int newEpisodDuration, double newRating) 
+        : name{newName}, numberOfEpisodes{newNumberOfEpisodes}, episodDuration{newEpisodDuration}, rating{newRating} {};
+    ~Serial() {};
 };
 
 class Anime {
 private:
     string name;
-    int episodDuration;
     int numberOfEpisodes;
+    int episodDuration;
     string studio;
-    set<string> genres;
     double rating;
-public:
+public: 
     void setName (string &newName) {
         name = newName;
     }
@@ -59,19 +83,51 @@ public:
         rating = newRating;
     }
     string getStudio () {
-        cout << "studio was getted, value = " << studio; 
+        cout << ">>> Studio was getted, value = " << studio; 
         return studio;
     }
     void getAll () {
-        cout << "Name: " << name 
-             << 
+        cout << "Name: " << name << endl
+             << "Episodes: " << numberOfEpisodes << endl
+             << "Duration: " << episodDuration << endl
+             << "Studio: " << studio << endl
+             << "Rating: " << rating << endl;
              
     }
+
+    Anime () 
+        : Anime{"None", 0, 0, "None", 0.0} {};
+    Anime (string newName) 
+        : Anime{newName, 0, 0, "None", 0.0} {};
+    Anime (string newName, int newNumberOfEpisodes) 
+        : Anime{newName, newNumberOfEpisodes, 0, "None", 0.0} {};
+    Anime (string newName, int newNumberOfEpisodes, int newEpisodDuration) 
+        : Anime{newName, newNumberOfEpisodes, newEpisodDuration, "None", 0.0} {};
+    Anime (string newName, int newNumberOfEpisodes, int newEpisodDuration, string newStudio) 
+        : Anime{newName, newNumberOfEpisodes, newEpisodDuration, newStudio, 0.0} {};
+    Anime(string newName, int newNumberOfEpisodes, int newEpisodDuration, string newStudio, double newRating) 
+        : name{newName}, numberOfEpisodes{newNumberOfEpisodes}, episodDuration{newEpisodDuration}, studio{newStudio}, rating{newRating} {};
+    ~Anime() {};
 };
 
 
 
 int main () {
-    cout << "Hello!" << endl;
+    Anime jojo("JoJo", 24, 20, "davidProduction", 8.8);
+    jojo.getAll();
+    cout << endl;
+    jojo.setRating(7.88);
+    cout << endl;
+    jojo.getAll();
+
+    cout << endl << "-----------------------------" << endl << endl;
+
+    Serial friends("Friends", 100, 45, 9.5);
+    friends.getAll();
+
+    cout << endl << "-----------------------------" << endl << endl;
+
+    Film duna("Duna", 150, 8.0);
+    duna.getAll();
     return 0;
 }
